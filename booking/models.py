858644aae338652cd1f77625eca274df5664a5a4
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-09-25 21:57:43 +0000

    Add data to models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+time_options = (
+    ("09:00", "09:00"),
+    ("10:00", "10:00"),
+    ("11:00", "11:00"),
+    ("12:00", "12:00"),
+    ("13:00", "13:00"),
+    ("14:00", "14:00"),
+    ("15:00", "15:00"),
+)
+
+GUEST_CAPACITY = (
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+)
+
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.get_full_name() if self.user.get_full_name() else self.user.username
+
+
+class Booking(models.Model):
+    guest = models.CharField(max_length=10, choices=GUEST_CAPACITY, default="1")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=time_options, default="09:00")
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="bookings")
+    comment = models.TextField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"Booking for {self.customer} at {self.day} {self.time}"
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Customer, Booking
+from django.views import generic
 
 # Create your views here.
+
+class HomePage(generic.ListView):
+    model = Booking
+    template_name = 'index.html'
+
+
+# View to display available tables
+#def available_booking_times(request):
+    
+# View to make a reservation
+class MakeBooking(Booking):
+    model = Booking
+
+# View to display booking details
+#def booking_details(request, booking_id):
