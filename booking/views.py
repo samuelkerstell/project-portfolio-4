657e76b21e3c7086e5af220ef6df288d1cf43952
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-10-07 06:07:51 +0000

    -

--- booking/views.py
+++ booking/views.py
@@ -8,23 +8,20 @@ from django.contrib import messages
 from django.db import IntegrityError
 from datetime import date
 
-
-
-
+# View for home page
 class HomePage(generic.TemplateView):
     template_name = 'index.html'
 
 
+# View for menu page
 class MenuPage(generic.TemplateView):
     template_name = 'menu.html'
 
  
-# View to make a reservation
+# Code inspired from this https://www.geeksforgeeks.org/createview-class-based-views-django/?ref=lbp
 class MakeBooking(generic.CreateView, LoginRequiredMixin):
-    # specify the model for create view
     model = Booking
     template_name = 'booking_form.html'
-    # specify the fields to be displayed
     fields = ['guests', 'time', 'day', 'comment']
     
     def form_valid(self, form):
@@ -33,14 +30,10 @@ class MakeBooking(generic.CreateView, LoginRequiredMixin):
         if start_date < date.today():
             messages.error(self.request, 'Booking date cannot be in the past.')
             return self.form_invalid(form)
-        
-        # Connect the booking to the logged-in user
+    
         form.instance.customer = self.request.user
-        
-        # Set a custom flag in the session to indicate successful confirmation
         self.request.session['booking_success'] = True
 
-        # Call the parent class's form_valid method to save the booking
         try:
             return super().form_valid(form)
         except IntegrityError:
@@ -49,11 +42,12 @@ class MakeBooking(generic.CreateView, LoginRequiredMixin):
             return self.form_invalid(form)
 
     def get_success_url(self):
-        # Redirect to the booking details page for the newly created booking
+        # Redirects to the booking details page for the newly created booking
         return reverse('booking_details')
 
 
 # View to display booking details
+# Code inspired from this https://www.geeksforgeeks.org/listview-class-based-views-django/
 class BookingDetails(generic.ListView, LoginRequiredMixin):
     model = Booking
     template_name = 'booking_details.html'
@@ -62,10 +56,8 @@ class BookingDetails(generic.ListView, LoginRequiredMixin):
         context = super().get_context_data(**kwargs)
         success_message = None
 
-        # Check if the custom flag exists in the session
         if self.request.session.get('booking_success'):
             success_message = 'Your booking has been confirmed.'
-            # Clear the custom flag
             del self.request.session['booking_success']
 
         context['success_message'] = success_message
@@ -73,6 +65,7 @@ class BookingDetails(generic.ListView, LoginRequiredMixin):
 
     def get_queryset(self):
         return Booking.objects.filter(customer=self.request.user).order_by('day', 'time')
+
 
 # View to delete bookings
 class DeleteBooking(generic.DeleteView, LoginRequiredMixin):
@@ -83,10 +76,13 @@ class DeleteBooking(generic.DeleteView, LoginRequiredMixin):
 
     def get_queryset(self):
         return Booking.objects.filter(pk=self.kwargs['pk'], customer=self.request.user)
+
+
+# View to update existing booking
+# Code inspired from this https://www.geeksforgeeks.org/updateview-class-based-views-django/
 class UpdateBooking(generic.UpdateView, LoginRequiredMixin):
     model = Booking
     template_name = "update_booking.html"
-    # specify the fields to be displayed
     fields = ['guests', 'time', 'day', 'comment']
     success_url = "/booking_details"
 
@@ -103,4 +99,3 @@ def handler404(request, exception, template_name="404.html"):
 
 def handler500(request, *args, **argv):
     return render(request, '500.html', status=500)
-
