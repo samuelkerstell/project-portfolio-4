8e7a9f8d0a09fba18a573683e5459512f8167169
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-10-07 09:28:56 +0000

    -

--- project4/settings.py
+++ project4/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'django.contrib.staticfiles',
+    'cloudinary',
     'booking',
 ]
 
@@ -141,9 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 
 # Default primary key field type
