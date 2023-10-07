fbeb5943ce9b0ccf01fc1ab1b8a4be10dd8de036
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-10-07 07:57:16 +0000

    -

--- project4/settings.py
+++ project4/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
